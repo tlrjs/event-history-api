9cdb22f09b24392b9848007811874d6732c4c3e4
Tyler Shipe <tjshipe@gmail.com>
2021-04-09 14:14:03 -0400

    add sentry

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
+import * as Sentry from '@sentry/node';
 import { createConnection } from 'typeorm';
-import Airbrake from '@airbrake/node';
 import { fetchTradesByOwner, fetchTradesByOpenOrders } from './fetchTrades';
 
-new Airbrake.Notifier({
-  projectId: 328958,
-  projectKey: 'f853a43ca28598a71f135a84848368d5',
-  environment: 'production',
+Sentry.init({
+  dsn: 'https://68333cb984fc43caa598b6a86da05f01@o568322.ingest.sentry.io/5713285',
 });
 
 createConnection().then(async (db) => {
   const app = express();
+
+  app.use(Sentry.Handlers.requestHandler());
   app.use(express.json(), cors());
 
   app.get('/trades/owner/:ownerAddress', async (req, res) => {
@@ -43,6 +43,8 @@ createConnection().then(async (db) => {
       res.status(500).send({ message: 'error', error });
     }
   });
+
+  app.use(Sentry.Handlers.errorHandler());
 
   app.listen(process.env.PORT, () =>
     console.log(`Server listening at http://localhost:${process.env.PORT}`)
